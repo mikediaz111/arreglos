e64cad704062e23cda53383056575dbd0cf3c51a
Miguel <miguel.diaz5@catolica.edu.sv>
2021-03-04 23:04:51 -0600

    primer commit

---
+++ Module1.vb
@@ -0,0 +1,11 @@
+﻿Module Module1
+    Sub Main()
+        Dim cadena1 As String = "Bienvenidos a Clase de Programacion Orientada a Objetos"
+        Dim cadena2 As String = "Bienvenidos a clase de Programacion Orientada a Objetos"
+        Console.WriteLine(cadena1.ToUpper())
+        Console.WriteLine(cadena1.ToLower())
+        Console.WriteLine(cadena1.Substring(6, 5))
+        Console.WriteLine(cadena1.Length())
+        Console.WriteLine(cadena1.Equals(cadena2))
+    End Sub
+End Module
